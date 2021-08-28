b900931530f470176d7ec59090e68a4d7e9d0037
Techfay <85510644+Techfay@users.noreply.github.com>
2021-08-28 17:39:40 +0530

    Comments in python.
    Comments can be used to explain Python code. Comments can be used to make the code more readable. Comments can be used to prevent execution when testing code.
    Comments start with a #, and Python will ignore them. Comments can be placed at the end of a line, and Python will ignore the rest of the line. A comment does not have to be text that explains the code, it can also be used to prevent Python from executing code.

---
+++ comment.py
@@ -0,0 +1,19 @@
+#This is a comment.
+print("Hello, World!")
+print("Hello, Shivangi") #This is a comment.
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#This is a comment
+#written in
+#more than just one line.
+#Multiline comment
+print("That's great.") 
+
+#Multiline comments by using Triple quotes
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Wow its so beautiful") 
